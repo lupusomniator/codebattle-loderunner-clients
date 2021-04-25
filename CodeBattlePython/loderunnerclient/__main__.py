45bf2bf21308bc81fc88b908fbf92917420d6a66
Stepan Polokhin <ilikepugs@yandex-team.ru>
2021-04-25 15:10:55 +0700

    OK

--- CodeBattlePython/loderunnerclient/__main__.py
+++ CodeBattlePython/loderunnerclient/__main__.py
@@ -1,9 +1,11 @@
 import logging
 from loderunnerclient.game_client import GameClient
-from loderunnerclient.environment import Environment
+# from loderunnerclient.environment import Environment
 from loderunnerclient.internals.board import Board
 from loderunnerclient.internals.actions import LoderunnerAction
 from loderunnerclient.game import Game
+from loderunnerclient.graph.dynamic_action_graph import DynamicActionGraph
+from loderunnerclient.bots.greedy_ant_bot import Ant
 import numpy as np
 
 
@@ -12,13 +14,23 @@ logging.basicConfig(format="%(asctime)s %(levelname)s:%(message)s", level=loggin
 def turn(board):
     board.save_to_file("last_board")
 
+def local_main():
+    board = Board.load_from_file("last_board")
+    dag = DynamicActionGraph(board, 10)
+    
+    my_pos = board.get_my_position()
+    ant = Ant(my_pos, dag, board)
+
+    ant.walk(10)
+    print(ant.action_sequence)
+
 def server_main():
-    env = Environment()
+    # env = Environment()
     gcb = GameClient(
         # change this url to your
         "https://dojorena.io/codenjoy-contest/board/player/dojorena392?code=407418550408423703"
     )
-    gcb.run(env.on_turn)
+    # gcb.run(env.on_turn)
 
 
 if __name__ == "__main__":
@@ -26,4 +38,5 @@ if __name__ == "__main__":
     # for i in range(10000):
         # turn(Board.load_from_file("last_board"))
     #main()
-    server_main()
+    # server_main()
+    local_main()
